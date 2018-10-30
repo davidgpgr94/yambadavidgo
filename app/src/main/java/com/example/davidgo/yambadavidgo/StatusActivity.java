d82feebe37eea219621b2e1c509044ddaed8d018
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-10-30 16:54:52 +0100

    Added permissions for Internet access. And started to use twitter4j

--- app/src/main/java/com/example/davidgo/yambadavidgo/StatusActivity.java
+++ app/src/main/java/com/example/davidgo/yambadavidgo/StatusActivity.java
@@ -7,11 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class StatusActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "StatusActivity";
     EditText et_status;
     Button button_tweet;
+    Twitter twitter;
 
 
     @Override
@@ -22,6 +27,15 @@ public class StatusActivity extends AppCompatActivity implements View.OnClickLis
         et_status = (EditText) findViewById(R.id.et_status);
         button_tweet = (Button) findViewById(R.id.button_tweet);
         button_tweet.setOnClickListener(this);
+
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.setOAuthConsumerKey("LA CONSUMER KEY")
+                .setOAuthConsumerSecret("EL CONSUMER SECRET")
+                .setOAuthAccessToken("EL ACCESS TOKEN")
+                .setOAuthAccessTokenSecret("EL ACCESS TOKEN SECRET");
+        TwitterFactory factory = new TwitterFactory(builder.build());
+        twitter = factory.getInstance();
+
     }
 
 
@@ -29,5 +43,13 @@ public class StatusActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         String status = et_status.getText().toString();
         Log.d(TAG, "onClicked");
+
+        try {
+            twitter.updateStatus(status);
+            Log.d(TAG, "Enviado correctamente: ");
+        } catch (Exception e) {
+            Log.e(TAG, "Fallo en el envio");
+            e.printStackTrace();
+        }
     }
 }
